02d2110d9b1d852adfa96bfa79479eafcb39ca25
github_username <bishalmajhi1997@gmail.com>
2022-08-29 17:48:29 +0530

    somebasics

--- basic1.py
+++ basic1.py
@@ -1,1 +1,63 @@
-print("bishal")
+#Data types
+#1.integer
+#int,or integer ,is a whole number ,positive or negative,without decimals ,of unlimited length.
+x=5
+print(type(x))
+#2.string
+#A string is a series of charecters.Anything inside quotes is a string.
+x="i am learning python"
+print(type(x))
+#3.float
+#The float type in python represents the floating point number.Float is used to represent real numbers and is written with a decimal point dividing the integer and fractional parts.4
+
+x=2
+y=5.32993
+print(type(y))
+#4.boolean
+#this is built in data type.it's used to represent the truth value of an expression.
+k=x>y
+print(type(k))
+#5.list(mutable=[])
+#A LSIT is a mutable,changeable ,prdered sequenece of elements.
+x=[5,6,7,8,9,10,11,12,13]
+print(type(x))
+a=["haseena","sony","monika","abdul"]
+print(type(a))
+#6.tuple(immutable=())
+#immutable,are used to store multiple items in a single variable.
+a=("haseena","sony","monika","abdul")
+print(type(a))
+
+#oprators
+#7.ASSIGNMENT operator :used in python to assign values to variables
+#8.Arithmetics opearator
+#9.Comparision opearator:compares two values and returns a boolean value,either True or False.
+#indention:indention refers to the space at the beginning of a code line.it uses indentation to indicate a block of code.
+#High level language:High level language are in widespread use.e.g.BASIC,C,C++,COBOL,FORTRAN,JAVA ,PYTHON.
+#low level language:provides little or no abstraction of programming concepts and is very to writting actual machine instructions.e.g.assembly and machine code
+#
+
+x="bishal"
+k="bkumsj"
+print(x<=k)
+a=5
+while(a !=0):
+    a-=1#this is assignment means a=a-1
+
+print(a)
+
+#it works from left to right
+# code left to right reading
+
+
+# from right to left
+# assigning someting always goes right to left
+
+a =4
+b=6
+
+c = a+b
+b=0
+b=c
+#A compiler translates the entire source code in a single run and An interpreter translates the entire source code line by line
+
