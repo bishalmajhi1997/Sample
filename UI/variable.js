459c430325d03e2123949fcf4fdd7967b473b964
github_username <bishalmajhi1997@gmail.com>
2022-10-08 11:40:24 +0530

    sggsskamaa

---
+++ UI/variable.js
@@ -0,0 +1,46 @@
+var empsalary;
+var myage;
+var empno=300;
+var empno=400;
+console.log(empno);
+// window.alert("Be alert.")
+var x1=300;
+console.log(x1);
+// console.log(names)undefined
+var names="rupa";
+console.log(names)
+let empsalarry=100000;
+console.log(empsalarry);
+const bosssalary=17988777;
+console.log(bosssalary);
+var s=2999999;
+const empdata="01dightt";
+console.log(empdata);
+var x=5;
+var y=6;
+var sum=x+y;
+console.log(sum)
+var a=100;
+console.log(a);
+var b=10.14444;
+console.log(b);
+var c="javascript"
+console.log(c)
+var d="JAVA SCRIPT"
+console.log(d)
+var e=true
+console.log(e)
+var f=false;
+console.log(f)
+var j,k=10;
+console.log(j);
+console.log(k);
+var m=100;
+var n=200;
+var z=m%n;
+console.log(z)
+var d=30,e=40;
+d+=1;
+d+=e;
+console.log(d)
+
