fcf57cfcd85bf27e7a12fcda53a6a96a227a934d
bishal majhi <bishalmajhi1997>
2023-03-02 16:06:17 +0530

    dl

--- p_language/restapi2/gs41/api/serializers.py
+++ p_language/restapi2/gs41/api/serializers.py
@@ -9,7 +9,7 @@ class SingerSerializer(serializers.ModelSerializer):
     # song=serializers.PrimaryKeyRelatedField(many=True,read_only=True)
     # song=serializers.HyperlinkedRelatedField(many=True,read_only=True,view_name='song-detail')
     # song=serializers.SlugRelatedField(many=True,read_only=True,slug_field='title')
-    song=serializers.HyperlinkedIdentityField(view_name='song-detail')
+    # song=serializers.HyperlinkedIdentityField(view_name='song-detail')
     class Meta:
         model=Singer
         fields=['id','name','gender','song']
